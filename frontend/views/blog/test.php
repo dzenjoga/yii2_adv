2a5c0f7b352e7036733c4979435f4b96907420dc
odmin <dmitriy.burin@gmail.com>
2016-09-27 15:59:27 +0600

    Init

---
+++ frontend/views/blog/test.php
@@ -0,0 +1,32 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Yii Application';
+$test_message = $header;
+?>
+<div class="site-index">	
+    <div class="jumbotron"> 
+        <h1><?= $test_message ?></h1>
+
+        <p class="lead">You have successfully created your Yii-powered application.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+             
+
+             
+                        Hello! It's a test view!
+
+
+               
+            </div>
+        </div>
+
+    </div>
+</div>
